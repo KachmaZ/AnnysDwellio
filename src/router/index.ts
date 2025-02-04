3303654892af42e17229d63f088873c611a707aa
Arthur Kachmazov <arthurkachmazov@yandex.ru>
2025-02-04 14:09:00 +0300

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/aparts',
+      name: 'apartsList',
+      component: () => import('@/views/ListView.vue'),
+    },
+    {
+      path: '/aparts/:id',
+      name: 'apartPage',
+      component: () => import('@/views/ApartView.vue'),
+    },
+    {
+      path: '/map',
+      name: 'map',
+
+      component: () => import('@/views/MapView.vue'),
+    },
+    {
+      path: '/in-progress',
+      name: 'inProgress',
+
+      component: () => import('@/views/InProgress.vue'),
+    },
+  ],
+})
+
+export default router
